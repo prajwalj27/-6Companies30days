185a68d8e652f8b473b42474bcf209062574b764
Prajwal Jaiswal <prajwal.jaiswal@hotmail.com>
2022-01-05 20:15:38 +0530

    Add solution for GCD of Strings

---
+++ 01-Goldman-Sachs/GcdForStrings.java
@@ -0,0 +1,36 @@
+/*
+Problem Link: https://leetcode.com/problems/greatest-common-divisor-of-strings/
+Title: Greatest Common Divisor of Strings
+Difficulty: Easy
+Language: Java
+*/
+
+public class GcdForStrings {
+
+  static String gcdOfStrings(String str1, String str2) {
+    // for the solution we need length of str2 to be less than str1,
+    // if length of str1 < str1, we swap str1 with str2 
+    if (str1.length() < str2.length()) {
+      return gcdOfStrings(str2,str1);
+
+    // if the str1 and str2 totally different strings
+    }else if (!str1.startsWith(str2)) {
+      return "";
+
+    }else if (str2.length() == 0) {
+      return str1;
+
+    // use the same logic as gcd of integers for the strings
+    }else {
+      return gcdOfStrings(str1.substring(str2.length()),str2);
+    }
+  }
+
+  public static void main(String[] args) {
+    String str1, str2;
+
+    str1 = "ABABAB";
+    str2 = "ABAB";
+    System.out.print(gcdOfStrings(str1, str2));
+  }
+}
