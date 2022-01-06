5fa22a46d39c7a9e1890e07bdecedf9059e244e5
Prajwal Jaiswal <prajwal.jaiswal@hotmail.com>
2022-01-07 00:18:03 +0530

    Add Solution for Q. Find 10 largest nos. from 10M entries

---
+++ 01-Goldman-Sachs/MaxOfTenMillion.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+public class MaxOfTenMillion {
+  static int[] kLargest(Integer[] arr, int k) {
+    // sort the given entries in descending order 
+    Arrays.sort(arr, Collections.reverseOrder());
+
+    // return the first k entries as result
+    int[] result = new int[k];
+    for (int i = 0; i < k; i++) {
+      result[i] = arr[i];
+    }
+
+    return result;
+  }
+
+
+  public static void main(String[] args) {
+    Integer[] arr = {12, 123, 3243, 1, 423 ,324 ,23, 2, 3, 23, 4, 5, 45645, 74, 37};
+    int k = 10;
+
+    int[] result = kLargest(arr, k);
+    System.out.println(Arrays.toString(result));
+    
+  }
+  
+}
