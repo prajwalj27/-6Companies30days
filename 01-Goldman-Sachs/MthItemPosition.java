3e45aaece3d7ec4e1b66e4df636e545929e314cb
Prajwal Jaiswal <prajwal.jaiswal@hotmail.com>
2022-01-06 15:33:20 +0530

    Add comments explaining the algorithm

--- 01-Goldman-Sachs/MthItemPosition.java
+++ 01-Goldman-Sachs/MthItemPosition.java
@@ -9,9 +9,14 @@ public class MthItemPosition {
 
   static int findPosition(int n, int m, int k) {
 
+    // n-(k-1) is the number of postitions we need to cover from the starting
+    // position k to the beginning of the circle i.e the 1st position.
+    // If m is less than this value, we can return m+(k-1) 
     if (m <= n - k +1) 
       return m + k - 1;
     
+    // now we need to find the remaining no. of items to be distributed 
+    // before we reach the 1st position of the circle
     m = m - (n - k + 1);
 
     return (m % n == 0) ? n : m % n;
