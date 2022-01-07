18903cb7d082da2f5b8b7bef9385be98fc56a9a6
Prajwal Jaiswal <prajwal.jaiswal@hotmail.com>
2022-01-07 23:02:29 +0530

    Add Solution for Q. Find ith Ugly Number

---
+++ 01-Goldman-Sachs/UglyNumbers.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class UglyNumbers {
+
+    static long nthUglyNumber(int n)
+    {
+
+        TreeSet<Long> t = new TreeSet<>();
+        t.add(1L);
+        int i = 1;
+        while (i < n) {
+            long temp = t.pollFirst();
+            t.add(temp * 2);
+            t.add(temp * 3);
+            t.add(temp * 5);
+            i++;
+        }
+
+        return t.pollFirst();
+    }
+
+    public static void main(String[] args)
+    {
+        int n = 150;
+        System.out.println(nthUglyNumber(n));
+    }
+}
